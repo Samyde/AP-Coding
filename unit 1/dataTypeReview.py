6ec849868effea6fc0fbc982a39c77b90376b86b
Samyde <smcneil900@boyslatin.net>
2025-08-22 15:09:18 +0000

    data type

---
+++ unit 1/dataTypeReview.py
@@ -0,0 +1,17 @@
+ print('Samyde McNeil')
+
+# W3SChools.com - our frined
+
+# 1.
+ # print each of the the following data types
+ # string 
+ # boolean
+ # interprint("It's alright")
+ # float
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+# 2.
+# create and print a list that features 2 of each data typess.
