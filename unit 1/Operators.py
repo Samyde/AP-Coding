5729fdf84147cb3323126ac318d246e7839b6027
Samyde <smcneil900@boyslatin.net>
2025-09-03 15:12:40 +0000

    done

---
+++ unit 1/Operators.py
@@ -0,0 +1,67 @@
+print('Good Morning')
+username = 'Samyde'
+function goodMorningMsg()()
+def goodMorningMsg():
+
+ print('good morning'+ username )
+
+ goodMorningMsg()
+
+ def compoundIntrest():
+      investmentAmount = 100
+      moneyGained = 0
+      print(moneyGained)
+
+print(340,000)
+
+a = ("h", "b", "a", "c", "f", "d", "e", "g")
+x = sorted(a)
+print(x)
+
+def myfunc(n):
+  return abs(10-n)
+
+a = (5, 3, 1, 11, 2, 12, 17)
+x = sorted(a, key=myfunc)
+print(x)
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
--- unit 1/dataTypeReview.py
+++ unit 1/dataTypeReview.py
@@ -15,3 +15,24 @@ print('He is called "Johnny"')
 
 # 2.
 # create and print a list that features 2 of each data typess.
+
+school="boys latin" #string
+phonenumber = 215321344 # interger
+isEnrolled = True # Boolean
+
+# - single equal sign - assignment operator
+
+# -- double equal sgin - comparison
+# != is not the same 
+# > greater then
+# < less then
+# <=
+#<=
+
+Print(2, >= 2 )
+
+# logical operator - used to compare conditions 
+print(2 > 1 and 3 == 3) # true
+print (5 * 3 or 28 < 18)# true
+
+# arltmeatic operater - 
