616936b5680fca9ee2a0a4cc8c781c5e27fed73d
Samyde <smcneil900@boyslatin.net>
2025-09-11 15:13:20 +0000

    done

---
+++ unit 1/algorithms.py
@@ -0,0 +1,23 @@
+# Create a function that will return the largest 
+# and the smallest an array 
+numbers ={0,1,5,18,20,39,48,83,89,72,90,18282,1292}
+# min / max
+
+def minMax():
+    x = min(numbers)
+    y = max(numbers)
+
+    print(x ,y) 
+
+minMax()
+
+# Create a functon that will reverse the characters in a string.
+# your string must be passed into the function as a parameter 
+# EXAMPLE: book = koob
+# car = rac 
+
+def reverseString(word)
+    text = word(::=1)
+    print(text)
+
+reverseString('anything')    
